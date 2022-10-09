fa90681abe2fcf59353801479b546a56cef68ff8
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-09 17:11:23 +0300

    added masha

---
+++ ifurmasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("your name"); 
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.WriteLine("WOW, it is Masha");
+}
+    else 
+{
+    Console.WriteLine ("Hello!");
+    Console.WriteLine(username);
+}
